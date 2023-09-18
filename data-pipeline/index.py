d3c577cca740d2c421828733757ca1ab7ccac283
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-09-18 16:53:22 +0200

    implement index.py

--- data-pipeline/index.py
+++ data-pipeline/index.py
@@ -28,86 +28,59 @@ def load_environment_vars() -> dict:
     return {"OPENAI_API_KEY": openapi_key, "WEAVIATE_URL": weaviate_url, "WEAVIATE_API_KEY": weaviate_api_key}
 
 
-def index_data():
-    """Index Data"""
-    #file_name = "langchain-github-issues-2021-09-22.jsonl"
-    #df = pd.read_json(file_name, orient="records", lines=True)
-    df = pd.read_pickle("./data-pipeline/langchain-github-issues-2023-09-18.pkl")
+def index_data(openapi_key: str, weaviate_url: str, weaviate_api_key: str):
+    """Index Data into Weaviate"""
+    file_name = "./data-pipeline/langchain-github-issues-2023-09-18.pkl"
 
-    #print(df)
+    logging.info(f"Loading data from '{file_name}'")
+    df = pd.read_pickle(file_name)
 
-    #logging.info("Indexing data from Documents...")
-    #embeddings = OpenAIEmbeddings()
-    #vector_store = Weaviate.from_documents(documents=docs, embedding=embeddings)
-    #logging.info("Indexing data from Documents - (OK)")
-    
-    #return vector_store
-    openapi_key = os.getenv("OPENAI_API_KEY")
-    weaviate_url = os.getenv("WEAVIATE_URL")
-    weaviate_api_key = os.getenv("WEAVIATE_API_KEY")
-
+    logging.info(f"Initializing Weaviate Client: '{weaviate_url}'")
     client = weaviate.Client(
         url=weaviate_url,
         auth_client_secret=weaviate.AuthApiKey(api_key=weaviate_api_key), 
         additional_headers={"X-OpenAI-Api-Key": openapi_key})
     
+    logging.info(f"Creating 'GituHubIssue' schema in Weaviate: '{weaviate_url}'")
     client.schema.delete_class("GitHubIssue")
-    
     class_obj = {
-    "class": "GitHubIssue",
-    "vectorizer": "text2vec-openai",
-    "moduleConfig": {
-        "text2vec-openai": {
-            "model": "ada",
-            "modelVersion": "002",
-            "type": "text"
+        "class": "GitHubIssue",
+        "vectorizer": "text2vec-openai",
+        "moduleConfig": {
+            "text2vec-openai": {
+                "model": "ada",
+                "modelVersion": "002",
+                "type": "text"
+            }
         }
     }
-    }
-
     client.schema.create_class(class_obj)
 
-    current_issue = None
-
-    # Iterate through each row of data
+    logging.info(f"Importing data to Weaviate: '{weaviate_url}'")
     try:
-        with client.batch as batch:  # Initialize a batch process
-            batch.batch_size = 100
-            for issue in df.itertuples():
-                current_issue = issue
-
+        with client.batch as batch: 
+            batch.batch_size = 50
+            for item in df.itertuples():
                 properties = {
-                    "title": issue.title,
-                    "url": issue.url,
-                    "labels": issue.labels,
-                    "description": issue.description,
+                    "title": item.title,
+                    "url": item.url,
+                    "labels": item.labels,
+                    "description": item.description,
                 }
 
-                batch.add_data_object(data_object=properties, class_name="GitHubIssue")
-    except Exception as e:
-        print(f"something happened {e}. Failure at {current_issue}")
-
-    #some_objects = client.data_object.get()
-    #print(json.dumps(some_objects))
-
-    # https://weaviate.io/developers/weaviate/tutorials/query
-    response = (
-        client.query
-        .get("GitHubIssue", ["description", "title", "labels"])
-        .with_near_text({"concepts": ["documentation"]})
-        .with_limit(2)
-        .do()
-    )
-
-    print(json.dumps(response, indent=4))
+                batch.add_data_object(
+                    data_object=properties, 
+                    class_name="GitHubIssue")
+    except Exception as ex:
+        logging.error(f"Unexpected Error: {ex}")
+        raise
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
     try:
         env_vars = load_environment_vars()
-
-        index_data()
+        index_data(env_vars["OPENAI_API_KEY"], env_vars["WEAVIATE_URL"], env_vars["WEAVIATE_API_KEY"])
     except EnvironmentError as ee:
         logging.error(f"Environment Error: {ee}")
         raise
