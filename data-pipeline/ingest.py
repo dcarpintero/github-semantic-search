25e2378b8ee035b453243d4acc6783bac719d78c
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-09-18 14:56:46 +0200

    fetch and store github issues

--- data-pipeline/ingest.py
+++ data-pipeline/ingest.py
@@ -1,90 +1,54 @@
 from langchain.document_loaders import GitHubIssuesLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Weaviate
 from dotenv import load_dotenv
 import os
 import logging
+import pandas as pd
+
+
+GITHUB_REPOSITORY="langchain-ai/langchain"
+GITHUB_LABEL="langchain"
+STORE_PATH="data-pipeline"
 
 def load_environment_vars() -> dict:
     """Load required environment variables. Raise an exception if any are missing."""
     
     load_dotenv()
-    
     github_token = os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")
-    api_key = os.getenv("OPENAI_API_KEY")
-    weaviate_url = os.getenv("WEAVIATE_URL")
-    weaviate_api_key = os.getenv("WEAVIATE_API_KEY")
     
     if not github_token:
-        raise EnvironmentError("GITHUB_TOKEN environment variable not set.")
-    
-    if not api_key:
-        raise EnvironmentError("OPENAI_API_KEY environment variable not set.")
-    
-    if not weaviate_url:
-        raise EnvironmentError("WEAVIATE_URL environment variable not set.")
-    
-    if not weaviate_api_key:
-        raise EnvironmentError("WEAVIATE_API_KEY environment variable not set.")
+        raise EnvironmentError("GITHUB_PERSONAL_ACCESS_TOKEN environment variable not set.")
 
     logging.info("Environment variables loaded.")
-    return {"GITHUB_TOKEN": github_token, "OPENAI_API_KEY": api_key, "WEAVIATE_URL": weaviate_url, "WEAVIATE_API_KEY": weaviate_api_key}
+    return {"GITHUB_TOKEN": github_token}
 
 
-def initialize_github_loader() -> GitHubIssuesLoader:
-    """Load GitHub issues with comments and labels from the langchain-ai/langchain repository."""	
+def initialize_github_loader(repo: str) -> GitHubIssuesLoader:
+    """Init GitHubIssuesLoader."""	
 
+    logging.info(f"Initializing GitHubIssuesLoader for repo: {repo}")
     loader = GitHubIssuesLoader(
-        repo="langchain-ai/langchain",
+        repo=repo,
         include_prs=False,
-        include_comments=False,
-        include_labels=True,
-        creator="RoderickVM",
     )
-
     return loader
 
 
-def load_and_index_data(loader):
-    """Load and Index Data from GitHub Repository"""
+def fetch_as_df(loader: GitHubIssuesLoader) -> pd.DataFrame:
+    """Fetch Data from GitHub Repository"""
 
-    docs = load_data(loader)
-    
-    logging.info("Loaded %s documents from Github.", len(docs))
-    logging.info("*******************************************")
-    logging.info(docs[0].page_content)
-    logging.info("*******************************************")
-    logging.info(docs[0].metadata)
-    logging.info("*******************************************")
-    logging.info(docs[0].metadata["labels"])
-    logging.info("*******************************************")
-    logging.info(docs[0].metadata["state"])
-
-    index = index_data(docs)
+    logging.info(f"Fetching 'issues' from Github: '{loader.repo}'")
+    docs = loader.load()
+    df = pd.DataFrame.from_records(docs)
+    return df
 
 
-def load_data(loader: GitHubIssuesLoader) -> []:
-    """Load Data from GitHub Repository"""
+def store_as_json(df: pd.DataFrame, label: str, path: str):
+    """Store DataFrame as json to local file system"""
+    file_name = f"{label}-github-issues-{pd.Timestamp.today().strftime('%Y-%m-%d')}.jsonl"
 
-    logging.info("Loading data from Github: %s", loader.repo)
-    documents = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    docs = text_splitter.split_documents(documents)
-    logging.info("Loading data from Github - (OK)")
-        
-    return docs
+    logging.info(f"Storing 'issues' to '{path}/{file_name}'")
+    df.to_json(f"{path}/{file_name}", orient="records", lines=True)
 
-
-def index_data(docs: []) -> Weaviate:
-    """Index Data from Documents"""
-
-    logging.info("Indexing data from Documents...")
-    embeddings = OpenAIEmbeddings()
-    vector_store = Weaviate.from_documents(documents=docs, embedding=embeddings)
-    logging.info("Indexing data from Documents - (OK)")
-    
-    return vector_store
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
@@ -92,8 +56,12 @@ if __name__ == "__main__":
     try:
         env_vars = load_environment_vars()
 
-        loader = initialize_github_loader()
-        load_and_index_data(loader)
+        loader = initialize_github_loader(GITHUB_REPOSITORY)
+        df = fetch_as_df(loader)
+        store_as_json(df, label=GITHUB_LABEL, path=STORE_PATH)
+    except EnvironmentError as ee:
+        logging.error(f"Environment Error: {ee}")
+        raise
     except Exception as ex:
-        logging.error("Unexpected Error: %s", ex)
-        raise ex
+        logging.error(f"Unexpected Error: {ex}")
+        raise
